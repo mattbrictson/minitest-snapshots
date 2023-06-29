7c2563eeb775cf6ca32120f659e63562b8500e9c
Matt Brictson <matt@mattbrictson.com>
2023-06-29 12:02:59 -0700

    Add a simple test suite, with GitHub Actions workflow (#12)
    * Add test "suite"
    * Add Ruby versions to workflow matrix; upgrade checkout action
    * Upgrade bundler to fix DidYouMean::SPELL_CHECKERS warning
    * Remove extraneous bundler dependency
    * Add README badges
    
    ---------
    
    Co-authored-by: Harry Brundage <harry.brundage@gmail.com>

---
+++ test/minitest_snapshots_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class TestSnapshots < Minitest::Test
+  def test_string_matches
+    assert_matches_snapshot "foo"
+  end
+
+  def test_unmatching_string_raises
+    error = assert_raises(Minitest::Assertion) do
+      assert_matches_snapshot "foo"
+    end
+
+    assert_match(/does not match the snapshot/, error.message)
+  end
+
+  def test_hash_matches
+    assert_matches_snapshot({ foo: "bar", baz: 1, qux: { foo: "corge" } })
+  end
+
+  def test_set
+    assert_matches_snapshot(Set.new((0..100).sort_by { rand }))
+  end
+end
